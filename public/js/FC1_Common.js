42fe409f046943fd22f4bbd887bc65d918c75770
Dangtattest <dangtat.n20@gmail.com>
2023-03-01 21:54:45 +0700

    first commit

---
+++ public/js/FC1_Common.js
@@ -0,0 +1,16 @@
+////////////// YÊU CẦU DỮ LIỆU TỪ SERVER- REQUEST DATA //////////////
+var myVar = setInterval(myTimer, 100);
+function myTimer() {
+    socket.emit("Client-send-data", "Request data client");
+}
+ 
+// Hàm hiển thị dữ liệu lên IO Field
+function fn_IOFieldDataShow(tag, IOField, tofix){
+    socket.on(tag,function(data){
+        if(tofix == 0){
+            document.getElementById(IOField).value = data;
+        } else{
+        document.getElementById(IOField).value = data.toFixed(tofix);
+        }
+    });
+}
